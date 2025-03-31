07b2934af36f23107465afc0756530baadc84d45
JohnathanKhoa <johnathan.k.nguyen.95@gmail.com>
2025-03-31 11:13:12 -0700

    fix small screen loading animations

--- components/spotify/ActiveTrackOverlay.tsx
+++ components/spotify/ActiveTrackOverlay.tsx
@@ -2,8 +2,8 @@ import { AudioLines } from "lucide-react";
 
 export default function ActiveTrackOverlay() {
   return (
-    <div className="absolute w-screen justify-items-end shadow-2xl">
-      <AudioLines className="animate-pulse translate-y-3 -translate-x-10" />
+    <div className="fixed w-screen justify-items-end shadow-2xl">
+      <AudioLines className="animate-pulse translate-y-3 md:-translate-x-10" />
     </div>
   );
 }
--- components/spotify/LoadingOverlay.tsx
+++ components/spotify/LoadingOverlay.tsx
@@ -2,8 +2,8 @@ import { Disc3 } from "lucide-react";
 
 export default function LoadingOverlay() {
   return (
-    <div className="absolute w-screen  justify-items-end shadow-2xl  ">
-      <Disc3 className="animate-pulse translate-y-3 -translate-x-10 " />
+    <div className="fixed w-screen justify-items-end shadow-2xl">
+      <Disc3 className="animate-pulse translate-y-3 md:-translate-x-10" />
     </div>
   );
 }

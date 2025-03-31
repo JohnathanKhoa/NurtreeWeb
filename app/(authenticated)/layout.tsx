7e91cda589734aeeef77d0658d3ec878e369f7fb
JohnathanKhoa <johnathan.k.nguyen.95@gmail.com>
2025-03-31 11:02:25 -0700

    fix font

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -2,8 +2,6 @@ import "@/app/globals.css";
 import { NextAuthProvider } from "@/providers/NextAuthProvider";
 import NavBar from "@/components/spotify/NavBar";
 import TrackPlayerProvider from "@/providers/TrackPlayerProvider";
-import { Nunito_Sans } from "next/font/google";
-const fontFamily = Nunito_Sans({ preload: true });
 export default function RootLayout({
   children,
 }: {
@@ -15,7 +13,7 @@ export default function RootLayout({
         <div className="grid grid-cols-10 gap-2 scrollbar-hide">
           <div className="flex flex-col h-full col-span-full overflow-hidden rounded-lg  scrollbar scrollbar-track-black">
             <NavBar />
-            <main className={fontFamily.className}>{children}</main>
+            <main>{children}</main>
           </div>
         </div>
       </TrackPlayerProvider>
